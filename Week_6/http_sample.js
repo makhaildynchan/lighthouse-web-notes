4ae7fb6ed9c80a368a5d78346c5837d5004785aa
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-26 21:49:03 -0600

    http

---
+++ Week_6/http_sample.js
@@ -0,0 +1,14 @@
+// var http=require('http')
+// var server=http.createServer((function(request,response)
+// {
+// 	response.writeHead(200,
+// 	{"Content-Type" : "text/plain"});
+// 	response.end("Hello World\n");
+// }));
+// server.listen(7000);
+
+var request = require("request");
+	request("http://www.google.com",function(error,response,body)
+	{
+		console.log(body);
+	});
